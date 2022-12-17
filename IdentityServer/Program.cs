8fc8994063555281f995e5cf1ba4a5ffceedf863
ProsTuha <tuhalf2002@mail.ru>
2022-12-17 16:57:31 +0300

    Configuration for is4

---
+++ IdentityServer/Program.cs
@@ -0,0 +1,32 @@
+using IdentityServer.Server;
+using IdentityServerHost.Quickstart.UI;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddIdentityServer(options =>
+{
+    //options.EmitStaticAudienceClaim = true;
+})
+    .AddInMemoryIdentityResources(Config.IdentityResources)
+    .AddInMemoryApiScopes(Config.ApiScopes)
+    .AddInMemoryApiResources(Config.ApiResources)
+    .AddInMemoryClients(Config.Clients)
+    .AddTestUsers(TestUsers.Users)
+    .AddDeveloperSigningCredential();
+
+var app = builder.Build();
+
+app.UseStaticFiles();
+app.UseRouting();
+
+app.UseIdentityServer();
+
+app.UseAuthorization();
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapDefaultControllerRoute();
+});
+
+app.Run();

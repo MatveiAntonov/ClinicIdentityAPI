6981585075560f9954faa75764fd5a3803f099a8
ProsTuha <tuhalf2002@mail.ru>
2022-12-20 18:54:16 +0300

    Added migrations for database

---
+++ IdentityServer.Server/Migrations/20221220082247_AddRolesToDb.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IdentityServer.Server.Migrations
+{
+    public partial class AddRolesToDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "6d506b42-9fa0-4ef7-a92a-0b5b0a123665", "68c16036-3ed1-469e-96c8-163f6e017c41", "Visitor", "VISITOR" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "c3a0cb55-ddaf-4f2f-8419-f3f937698aa1", "02937812-7356-4995-82f3-4e6efc9b19c4", "Administrator", "ADMINISTRATOR" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "6d506b42-9fa0-4ef7-a92a-0b5b0a123665");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "c3a0cb55-ddaf-4f2f-8419-f3f937698aa1");
+        }
+    }
+}

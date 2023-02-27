95f09a70f628ebab21091f337ad5b31c61149b44
ProsTuha <tuhalf2002@mail.ru>
2023-02-27 20:41:20 +0300

    Added fields for UserRegistrationModel

--- IdentityServer.Server/Entities/ViewModels/UserRegistrationModel.cs
+++ IdentityServer.Server/Entities/ViewModels/UserRegistrationModel.cs
@@ -7,26 +7,32 @@ namespace IdentityServer.Server.Entities.ViewModels
     public class UserRegistrationModel
     {
         [Required(ErrorMessage = "First Name is required")]
-        public string FirstName { get; set; }
+        public string FirstName { get; set; } = String.Empty;
 
         [Required(ErrorMessage = "Last Name is required")]
-        public string LastName { get; set; }
+        public string LastName { get; set; } = String.Empty;
 
-        public string MiddleName { get; set; }
+        public string MiddleName { get; set; } = String.Empty;
 
         [Required(ErrorMessage = "Birth Date is required")]
         public DateTime BirthDate { get; set; }
 
+        [Required(ErrorMessage = "Phone number is required")]
+        public string PhoneNumber { get; set; } = String.Empty;
+
         [Required(ErrorMessage = "Email is required")]
         [EmailAddress]
-        public string Email { get; set; }
+        public string Email { get; set; } = String.Empty;
+
+		[Required(ErrorMessage = "Please select a file")]
+		//[FileExtensions(Extensions = ".jpg,.jpeg,.png", ErrorMessage = "Please select a valid image file (.jpg, .jpeg, .png)")]
+		public IFormFile? PhotoFile { get; set; }
 
         [Required(ErrorMessage = "Password is required")]
         [DataType(DataType.Password)]
-        public string Password { get; set; }
+        public string Password { get; set; } = String.Empty;
         [DataType(DataType.Password)]
         [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
-        public string ConfirmPassword { get; set; }
-
+        public string ConfirmPassword { get; set; } = String.Empty;
     }
 }

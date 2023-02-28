553c21d3ec066149c5b5abd5ddbceb6923e1c776
ProsTuha <tuhalf2002@mail.ru>
2023-02-28 12:29:22 +0300

    Added configuration for User table

---
+++ IdentityServer.Server/EntityTypeConfigurations/UsersConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using IdentityServer.Server.Entities;
+
+namespace IdentityServer.Server.EntityTypeConfigurations
+{
+    public class UsersConfiguration : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.HasKey(user => user.Id);
+            builder.HasIndex(user => user.Id).IsUnique();
+            builder.HasOne(user => user.Photo)
+                .WithOne()
+                .HasForeignKey<User>(user => user.PhotoId);
+        }
+    }
+}

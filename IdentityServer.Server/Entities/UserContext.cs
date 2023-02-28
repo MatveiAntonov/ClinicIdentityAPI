1475321c6b8102de0a174e3fa1d2dcd30bab04d0
ProsTuha <tuhalf2002@mail.ru>
2023-02-28 12:26:58 +0300

    Added Photos table with configuration

--- IdentityServer.Server/Entities/UserContext.cs
+++ IdentityServer.Server/Entities/UserContext.cs
@@ -1,11 +1,14 @@
 ﻿using IdentityServer.Server.Entities.Configuration;
+using IdentityServer.Server.EntityTypeConfigurations;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace IdentityServer.Server.Entities
 {
-    public class UserContext : IdentityDbContext
+    public class UserContext : IdentityDbContext<User>
     {
+        public DbSet<Photo> Photos { get; set; }
+
         public UserContext(DbContextOptions<UserContext> options) 
             : base(options) { }
 
@@ -14,6 +17,8 @@ namespace IdentityServer.Server.Entities
             base.OnModelCreating(builder);
 
             builder.ApplyConfiguration(new RoleConfiguration());
+            builder.ApplyConfiguration(new UsersConfiguration());
+            builder.ApplyConfiguration(new PhotosConfiguration());
         }
     }
 }

8168ef89cca593ab29db611e06c4a4632b774222
ProsTuha <tuhalf2002@mail.ru>
2023-02-28 12:29:03 +0300

    Added configuration for Photo table

---
+++ IdentityServer.Server/EntityTypeConfigurations/PhotosConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using IdentityServer.Server.Entities;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+
+namespace IdentityServer.Server.EntityTypeConfigurations
+{
+    public class PhotosConfiguration : IEntityTypeConfiguration<Photo>
+    {
+        public void Configure(EntityTypeBuilder<Photo> builder)
+        {
+            builder.HasKey(photo => photo.Id);
+            builder.HasIndex(photo => photo.Id).IsUnique();
+            builder.Property(photo => photo.PhotoUrl)
+                .IsRequired();
+        }
+    }
+}

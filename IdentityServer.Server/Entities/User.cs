25b032fd796e601478ff7931954e9810df682cfb
ProsTuha <tuhalf2002@mail.ru>
2023-02-28 12:27:38 +0300

    Added User with additional fields

---
+++ IdentityServer.Server/Entities/User.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace IdentityServer.Server.Entities
+{
+    public class User : IdentityUser
+    {
+        public int? PhotoId { get; set; }
+        public Photo? Photo { get; set; }
+		public string? PhotoName { get; set; } = String.Empty;
+	}
+}

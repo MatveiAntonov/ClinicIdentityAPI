8ae844209ee028666da34ce4e87706b745d58358
ProsTuha <tuhalf2002@mail.ru>
2023-02-28 12:27:11 +0300

    Added Photo entity

---
+++ IdentityServer.Server/Entities/Photo.cs
@@ -0,0 +1,9 @@
+﻿namespace IdentityServer.Server.Entities
+{
+    public class Photo
+    {
+        public int Id { get; set; }
+        public string PhotoUrl { get; set; } = String.Empty;
+        public string PhotoName { get; set; } = String.Empty;
+    }
+}

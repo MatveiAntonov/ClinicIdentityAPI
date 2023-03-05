5f4142ca74fa6ab06d5bbb1f15f2ed288b326971
ProsTuha <tuhalf2002@mail.ru>
2023-03-05 18:34:43 +0300

    Added PhotoId field

--- IdentityServer.Server/Entities/Photo.cs
+++ IdentityServer.Server/Entities/Photo.cs
@@ -3,7 +3,8 @@
     public class Photo
     {
         public int Id { get; set; }
-        public string PhotoUrl { get; set; } = String.Empty;
+		public int PhotoId { get; set; }
+		public string PhotoUrl { get; set; } = String.Empty;
         public string PhotoName { get; set; } = String.Empty;
     }
 }

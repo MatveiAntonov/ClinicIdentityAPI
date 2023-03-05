8ed7f8d5c9ed2e7e77cb7c22b16e5934db56ad89
ProsTuha <tuhalf2002@mail.ru>
2023-03-05 18:37:05 +0300

    Added repository for photo entity

---
+++ IdentityServer.Server/Repositories/PhotoRepository.cs
@@ -0,0 +1,45 @@
+﻿using IdentityServer.Server.Entities;
+using IdentityServer.Server.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace IdentityServer.Server.Repositories
+{
+	public class PhotoRepository : IPhotoRepository
+	{
+		private readonly UserContext _userDbContext;
+		public PhotoRepository(UserContext userContext)
+		{
+			_userDbContext = userContext;
+		}
+		public async Task<bool> CreatePhoto(Photo photo)
+		{
+			try
+			{
+				_userDbContext.Photos.Add(photo);
+				_userDbContext.SaveChanges();
+				return true;
+
+			}
+			catch
+			{
+				return false;
+			}
+		}
+
+		public bool DeletePhoto(Photo photo)
+		{
+			try
+			{
+				_userDbContext.Photos.Remove(photo);
+				_userDbContext.SaveChanges();
+
+				return true;
+
+			}
+			catch
+			{
+				return false;
+			}
+		}
+	}
+}

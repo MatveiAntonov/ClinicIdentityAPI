881bf567648bb22c3644516e2d6808cdc4cc2e72
ProsTuha <tuhalf2002@mail.ru>
2023-03-05 18:36:08 +0300

    Added photo-repository interface

---
+++ IdentityServer.Server/Interfaces/IPhotoRepository.cs
@@ -0,0 +1,10 @@
+﻿using IdentityServer.Server.Entities;
+
+namespace IdentityServer.Server.Interfaces
+{
+	public interface IPhotoRepository
+	{
+		Task<bool> CreatePhoto(Photo photo);
+		bool DeletePhoto(Photo photo);
+	}
+}
